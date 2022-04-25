97ad72ae45465009138b4cae45fbc6df5707ec1a
priority3 <prioritycc@outlook.com>
2022-04-25 18:28:11 +0800

    feature:axios 封装

---
+++ src/api/model/testModel.ts
@@ -0,0 +1,11 @@
+export interface testRequest {
+  apiKey: string
+  area?: string
+  areaID?: string
+}
+export interface testResponse {
+  area: string
+  areaCode: string
+  areaid: string
+  dayList: any[]
+}
